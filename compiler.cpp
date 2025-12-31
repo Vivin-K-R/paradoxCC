047ea4b71886a1b27e3be2abc64f9bcf87e4c361
Vivin K R <vivinkr05@gmail.com>
2025-12-31 10:13:51 +0530

    Done with lexer, tokenizing based on numbers and identifiers

--- compiler.cpp
+++ compiler.cpp
@@ -12,3 +12,54 @@ enum Token{
 
 static std::string identifierStr;
 static double Numval;
+
+static int gettok(){
+    static int LastChar=' ';
+
+    //white spaces need to be skipped
+    while(isspace(LastChar)){
+        LastChar = getchar();
+    }
+
+    //identifier
+    if(isalpha(LastChar)){
+        identifierStr = LastChar;
+        while(isalnum(LastChar=getchar())){
+            identifierStr += LastChar;
+        }
+        if(identifierStr == "def") return tok_def;
+        if(identifierStr == "extern") return tok_extern;
+        return tok_identifier;
+    }
+
+    //Numeric values
+    if(isdigit(LastChar) || LastChar == '.'){
+        std::string NumStr;
+        do{
+            NumStr += LastChar;
+            LastChar = getchar();
+        }while(isdigit(LastChar) || LastChar == '.');
+
+        Numval = strtod(NumStr.c_str(),0);
+        return tok_number;
+    }
+
+    //comments
+    if(LastChar == '#'){
+        do{
+            LastChar = getchar();
+        }while(LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        //continue read after comment
+        if(LastChar != EOF) return gettok();
+    }
+
+    //final checking
+    if(LastChar == EOF) return tok_eof;
+
+    //if curr char is not anything in language or we defined, then
+    //return the ASCII
+    int currChar = LastChar;
+    LastChar = getchar();
+    return currChar;
+}
